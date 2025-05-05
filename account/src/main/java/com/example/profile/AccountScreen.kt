d6f833dbcd2b78c8e9644d1f5d2f8c1b0a78a222
Vafeen <666av6@gmail.com>
2025-05-05 19:05:31 +0300

    feat: account screen plug

--- account/src/main/java/com/example/profile/AccountScreen.kt
+++ account/src/main/java/com/example/profile/AccountScreen.kt
@@ -1,16 +1,21 @@
 package com.example.profile
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import com.example.common.android.ui.CustomTheme
 
 @Composable
 fun AccountScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(stringResource(com.example.android.R.string.account_screen))
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(
+            text = stringResource(id = com.example.android.R.string.account_screen),
+            color = CustomTheme.colors.text
+        )
     }
 
 }
