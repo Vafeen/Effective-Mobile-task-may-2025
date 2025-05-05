ad01aa6be6387e12c5d48eaa93729be05ac20927
Vafeen <666av6@gmail.com>
2025-05-05 19:36:51 +0300

    fix: remove some useless scaffolds

--- onboarding/src/main/java/com/example/onboarding/OnboardingScreen.kt
+++ onboarding/src/main/java/com/example/onboarding/OnboardingScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -40,41 +39,36 @@ fun OnboardingScreen(navigator: Navigator) {
         if (state.courses.size > 4) gridState.scrollToItem(4)
     }
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize()
-    ) { paddingValues ->
-        Column(
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier.padding(top = 140.dp),
+            text = "Тысячи курсов\nв одном месте", fontSize = 28.sp
+        )
+        LazyHorizontalStaggeredGrid(
+            state = gridState,
             modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .height(316.dp)
+                .padding(top = 32.dp),
+            rows = StaggeredGridCells.Fixed(4)
         ) {
-            Text(
-                modifier = Modifier.padding(top = 140.dp),
-                text = "Тысячи курсов\nв одном месте", fontSize = 28.sp
-            )
-            LazyHorizontalStaggeredGrid(
-                state = gridState,
-                modifier = Modifier
-                    .height(316.dp)
-                    .padding(top = 32.dp),
-                rows = StaggeredGridCells.Fixed(4)
-            ) {
-                items(items = state.courses) { course ->
-                    course.CourseCard()
-                }
+            items(items = state.courses) { course ->
+                course.CourseCard()
             }
-            Spacer(modifier = Modifier.weight(1f))
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .padding(horizontal = 15.dp)
-                    .padding(bottom = 56.dp),
-                onClick = { viewModel.toContinue() },
-                colors = ButtonDefaults.buttonColors(containerColor = ThemeColor.green)
-            ) {
-                Text(stringResource(R.string.continue_str), color = Color.White)
-            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(horizontal = 15.dp)
+                .padding(bottom = 56.dp),
+            onClick = { viewModel.toContinue() },
+            colors = ButtonDefaults.buttonColors(containerColor = ThemeColor.green)
+        ) {
+            Text(stringResource(R.string.continue_str), color = Color.White)
         }
     }
 }
--- sign_in/src/main/java/com/example/sign_in/SignInScreen.kt
+++ sign_in/src/main/java/com/example/sign_in/SignInScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,7 +15,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -47,87 +45,82 @@ fun SignInScreen(navigator: Navigator) {
     val context = LocalContext.current
     val viewModel: SignInViewModel = koinViewModel { parametersOf(navigator) }
     val state by viewModel.state.collectAsState()
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        containerColor = CustomTheme.colors.background
-    ) { innerPadding ->
-        Column(
+
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.sign_in),
+            fontSize = 28.sp,
             modifier = Modifier
-                .padding(innerPadding)
-                .padding(horizontal = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .align(Alignment.Start)
+                .padding(top = 140.dp, bottom = 28.dp),
+            color = CustomTheme.colors.text
+        )
+        FieldWithHintComponent(
+            text = state.email,
+            topName = stringResource(R.string.email),
+            hint = stringResource(R.string.test_email),
+            onTextChanged = viewModel::processEmail,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        FieldWithHintComponent(
+            text = state.password,
+            topName = stringResource(R.string.password),
+            hint = stringResource(R.string.enter_password),
+            onTextChanged = viewModel::processPassword,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Button(
+            enabled = state.isButtonSignInActive,
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(top = 24.dp),
+            onClick = viewModel::signIn,
+            colors = ButtonDefaults.buttonColors(containerColor = ThemeColor.green)
         ) {
-            Text(
-                text = stringResource(R.string.sign_in),
-                fontSize = 28.sp,
-                modifier = Modifier
-                    .align(Alignment.Start)
-                    .padding(top = 140.dp, bottom = 28.dp),
-                color = CustomTheme.colors.text
-            )
-            FieldWithHintComponent(
-                text = state.email,
-                topName = stringResource(R.string.email),
-                hint = stringResource(R.string.test_email),
-                onTextChanged = viewModel::processEmail,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            FieldWithHintComponent(
-                text = state.password,
-                topName = stringResource(R.string.password),
-                hint = stringResource(R.string.enter_password),
-                onTextChanged = viewModel::processPassword,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                visualTransformation = PasswordVisualTransformation()
-            )
+            Text(stringResource(R.string.sign_in), color = Color.White)
+        }
+        ActionsComponent(modifier = Modifier.padding(top = 16.dp))
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 32.dp),
+            thickness = 1.dp,
+            color = Color.Gray
+        )
+        Row(
+            modifier = Modifier.padding(top = 32.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
             Button(
-                enabled = state.isButtonSignInActive,
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .padding(top = 24.dp),
-                onClick = viewModel::signIn,
-                colors = ButtonDefaults.buttonColors(containerColor = ThemeColor.green)
+                modifier = Modifier.weight(1f),
+                onClick = { viewModel.openVk(context) },
+                colors = ButtonDefaults.buttonColors(containerColor = ThemeColor.vkColor)
             ) {
-                Text(stringResource(R.string.sign_in), color = Color.White)
+                Icon(
+                    painter = painterResource(R.drawable.vk),
+                    contentDescription = stringResource(R.string.vk)
+                )
             }
-            ActionsComponent(modifier = Modifier.padding(top = 16.dp))
-            HorizontalDivider(
-                modifier = Modifier.padding(vertical = 32.dp),
-                thickness = 1.dp,
-                color = Color.Gray
-            )
-            Row(
-                modifier = Modifier.padding(top = 32.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly
+            Spacer(modifier = Modifier.width(10.dp))
+            Button(
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                onClick = { viewModel.openOk(context) },
+                modifier = Modifier
+                    .weight(1f)
+                    .background(
+                        brush = Brush.verticalGradient(ThemeColor.okColors),
+                        shape = CircleShape
+                    )
+                    .clip(CircleShape)
             ) {
-                Button(
-                    modifier = Modifier.weight(1f),
-                    onClick = { viewModel.openVk(context) },
-                    colors = ButtonDefaults.buttonColors(containerColor = ThemeColor.vkColor)
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.vk),
-                        contentDescription = stringResource(R.string.vk)
-                    )
-                }
-                Spacer(modifier = Modifier.width(10.dp))
-                Button(
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                    onClick = { viewModel.openOk(context) },
-                    modifier = Modifier
-                        .weight(1f)
-                        .background(
-                            brush = Brush.verticalGradient(ThemeColor.okColors),
-                            shape = CircleShape
-                        )
-                        .clip(CircleShape)
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.ok),
-                        contentDescription = stringResource(R.string.ok)
-                    )
-                }
+                Icon(
+                    painter = painterResource(R.drawable.ok),
+                    contentDescription = stringResource(R.string.ok)
+                )
             }
         }
     }
