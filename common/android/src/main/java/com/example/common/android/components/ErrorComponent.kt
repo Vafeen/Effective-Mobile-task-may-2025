c81be7ad3f09f62237869af1c08d972bb7c5f72e
Vafeen <666av6@gmail.com>
2025-05-09 00:33:16 +0300

    fix: update pullToRefresh layout in MainScreen (#34)

--- common/android/src/main/java/com/example/common/android/components/ErrorComponent.kt
+++ common/android/src/main/java/com/example/common/android/components/ErrorComponent.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,22 +19,18 @@ import com.example.android.R
 import com.example.common.android.ui.CustomTheme
 
 @Composable
-fun ErrorComponent(modifier: Modifier = Modifier, errorMessage: String, refresh: () -> Unit) {
+fun ErrorComponent(modifier: Modifier = Modifier, errorMessage: String) {
     Column(
-        modifier.clickable { refresh() },
+        modifier.verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Icon(
-                modifier = Modifier.size(50.dp),
-                painter = painterResource(R.drawable.error),
-                contentDescription = stringResource(R.string.error),
-                tint = CustomTheme.colors.text
-            )
-            Text(errorMessage, color = CustomTheme.colors.text, fontSize = 20.sp)
-        }
+        Icon(
+            modifier = Modifier.size(50.dp),
+            painter = painterResource(R.drawable.error),
+            contentDescription = stringResource(R.string.error),
+            tint = CustomTheme.colors.text
+        )
+        Text(errorMessage, color = CustomTheme.colors.text, fontSize = 20.sp)
     }
 }
